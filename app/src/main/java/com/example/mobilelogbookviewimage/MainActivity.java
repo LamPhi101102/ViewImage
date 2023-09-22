e7edc376682658ce66b0742d426eb41268ed5cf1
LamPhi101102 <124247193+LamPhi101102@users.noreply.github.com>
2023-09-22 19:50:40 +0700

    Catch Errors and Allow select images from Resource or Drive

--- app/src/main/java/com/example/mobilelogbookviewimage/MainActivity.java
+++ app/src/main/java/com/example/mobilelogbookviewimage/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.mobilelogbookviewimage.databinding.ActivityMainBinding;
@@ -21,6 +26,7 @@ import com.google.android.material.button.MaterialButton;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,18 +37,19 @@ public class MainActivity extends AppCompatActivity {
     private List<String> encodedImages = new ArrayList<>();
     private List<Bitmap> originalImages = new ArrayList<>();
     private int currentImageIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setListeners();
-        //processIntentData(getIntent());
-        updateImageView();
     }
+
     private void setListeners() {
-        binding.layoutImage.setOnClickListener(v -> {
+        binding.selectFromDrive.setOnClickListener(v -> {
+            originalImages.clear();
+            encodedImages.clear();
             // Create an intent to open the document picker with multiple selections
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -50,9 +57,12 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
             pickImage.launch(intent);
         });
+        binding.selectFromResource.setOnClickListener(v -> {
+            loadImagesFromResources();
+        });
 
-        MaterialButton buttonNext = findViewById(R.id.buttonNext);
-        MaterialButton buttonPrevious = findViewById(R.id.buttonPrevious);
+        ImageView buttonNext = findViewById(R.id.buttonNext);
+        ImageView buttonPrevious = findViewById(R.id.buttonPrevious);
 
         buttonNext.setOnClickListener(view -> {
             if (!encodedImages.isEmpty()) {
@@ -62,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
                     currentImageIndex = 0;
                 }
                 updateImageView();
+            }else{
+                ShowToast(this, "There are any images");
+
             }
         });
 
@@ -73,10 +86,59 @@ public class MainActivity extends AppCompatActivity {
                     currentImageIndex = encodedImages.size() - 1;
                 }
                 updateImageView();
+            }else{
+                ShowToast(this, "There are any images");
             }
+        });
+        binding.clear.setOnClickListener(view -> {
+            originalImages.clear();
+            encodedImages.clear();
+            binding.imageProfile.setImageResource(android.R.color.transparent);
+            binding.textAddImage.setVisibility(View.VISIBLE);
+            currentImageIndex = 0;
+            ShowToast(this,"All Images are Deleted");
         });
     }
 
+    private void loadImagesFromResources() {
+        Resources resources = getResources();
+        // Specify the resource IDs of the images you want to load from the drawable folder
+        int resourceIndex = 1; // Start index for your resources
+        while (true) {
+            // Construct the resource name dynamically
+            String resourceName = "image" + resourceIndex;
+
+            // Get the resource ID
+            int resourceId = resources.getIdentifier(resourceName, "drawable", getPackageName());
+
+            // Check if the resource exists
+            if (resourceId != 0) {
+                // Resource found, you can use it
+                Bitmap bitmap = BitmapFactory.decodeResource(resources, resourceId);
+                originalImages.add(bitmap);
+                String encodedImage = encodedImage(bitmap); // Replace this with your encoding logic
+                encodedImages.add(encodedImage);
+
+                // Increment the resource index for the next iteration
+                resourceIndex++;
+            } else {
+                // Resource not found, exit the loop
+                break;
+            }
+        }
+        int indexImages = resourceIndex -1;
+        ShowToast(this,"Added " + indexImages + " Images from resources");
+
+        // Now, you have loaded and processed all images dynamically
+
+        // Display the first image
+        int currentImageIndex = 0;
+        updateImageView();
+    }
+
+    private void ShowToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 
     private void updateImageView() {
         if (!encodedImages.isEmpty() && currentImageIndex < encodedImages.size()) {
@@ -87,7 +149,8 @@ public class MainActivity extends AppCompatActivity {
             binding.textAddImage.setVisibility(View.GONE);
         } else {
             // No images to display
-            binding.imageProfile.setImageResource(android.R.color.transparent); // Clear the ImageView
+            binding.imageProfile.setImageResource(android.R.color.transparent);
+            binding.textAddImage.setVisibility(View.VISIBLE);// Clear the ImageView
         }
     }
 
@@ -109,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
                             Uri imageUri = result.getData().getData();
                             processSelectedImage(imageUri);
                         }
+                        ShowToast(this, "Added "+ originalImages.size()+ " Images From Drive");
                     }
                 }
             }
